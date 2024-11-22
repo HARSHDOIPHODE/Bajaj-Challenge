7694cef9249f46d30fcd91cb7e36cd87f7dad94c
harshdoiphode <harshdoiphode1308@gmail.com>
2024-11-22 18:44:42 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
         setParsedData(jsonData);
         setIsValid(true);
         console.log(parsedData);
-        axios.post("http://localhost:8000/bfhl",parsedData)
+        axios.post("https://bajaj-challenge-psi.vercel.app/bfhl",parsedData)
         .then((res)=>{
           setRes(res.data);
         })
